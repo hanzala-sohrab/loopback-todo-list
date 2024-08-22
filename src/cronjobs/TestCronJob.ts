56d60a3533a42338ea50fe264dbe3422edba35f6
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2024-08-22 16:22:14 +0530

    Interact with database inside the cronjob

--- src/cronjobs/TestCronJob.ts
+++ src/cronjobs/TestCronJob.ts
@@ -1,19 +1,27 @@
 import {CronJob, cronJob} from '@loopback/cron';
+import {repository} from '@loopback/repository';
+import {TodoRepository} from '../repositories';
 
 @cronJob()
 export class TestCronJob extends CronJob {
-  constructor() {
+  totalTodos: number;
+  constructor(
+    @repository(TodoRepository) public todoRepository: TodoRepository,
+  ) {
     super({
       name: 'job-A',
-      onTick: () => {
-        this.doJob();
+      onTick: async () => {
+        await this.doJob();
       },
       cronTime: '*/1 * * * *',
       start: true,
     });
+
+    this.totalTodos = 5;
   }
 
-  doJob() {
+  async doJob() {
     console.log(`${new Date()} - Job is running 🥳.`);
+    console.log(await this.todoRepository.findById(Math.floor(Math.random() * this.totalTodos + 1)));
   }
 }
